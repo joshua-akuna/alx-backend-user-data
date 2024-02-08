28f6f725299755aa309deab86f8a83f75a329673
Akuna Joshua <akunajoshua@gmail.com>
2024-02-08 05:29:03 +0100

    filtered_logger.py file refactored

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -66,7 +66,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db() -> mysql.connector.connection.MySQLConnection:
+def _get_db() -> mysql.connector.connection.MySQLConnection:
     '''Connects to a MySQL server
     '''
     db_user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
@@ -80,3 +80,17 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
                                       database=db_name)
 
     return db_conn
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """connects to the musql database
+    """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME') or "root"
+    passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
+    host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
+    return conn
