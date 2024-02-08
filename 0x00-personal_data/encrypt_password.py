a9ef77e8ecf618a81cf9fec34332feef3c3c6787
Akuna Joshua <akunajoshua@gmail.com>
2024-02-08 04:54:49 +0100

    encrypt_password.py file commit

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""The module defines the hash_password and is_valid functions"""
+
+import bcrypt
+
+
+def hash_password(passwd: str) -> bytes:
+    """ takes a password string and returns the hashed password
+        in bytes
+    """
+    pwd = passwd.encode()
+    pwd_hash = bcrypt.hashpw(pwd, brypt.gensalt())
+    return pwd_hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """checks if password is valid"""
+    return bcrypt.checkpw(password.encode(), hashed_password)
