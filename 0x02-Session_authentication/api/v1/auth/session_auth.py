a52c8a401d775cd3f44e300ead500b62db6f917a
Akuna Joshua <akunajoshua@gmail.com>
2024-02-14 08:57:00 +0100

    SessionAuth: implemented the create session instance function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
-from auth import Auth
+import uuid
+from .auth import Auth
 
 
 class SessionAuth(Auth):
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = "Nine") -> str:
+        '''Creates a session id for the user id
+        '''
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        uid = uuid.uuid4()
+        self.user_id_by_session_id[str(uid)] = user_id
+        return str(uid)
