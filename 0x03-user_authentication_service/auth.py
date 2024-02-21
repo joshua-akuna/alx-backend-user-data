05ae9987078f1be2c8074e071fd9165342bbf185
Akuna Joshua <akunajoshua@gmail.com>
2024-02-21 08:14:00 +0100

    auth.py: update password method implemented

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -155,3 +155,19 @@ class Auth():
         else:
             user.reset_token = _generate_uuid()
             return user.reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """updates the user password and reset token field
+
+        Args:
+            reset_token (str): reset token of the user to update
+            password (str): new password of the user to update to
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError
+        else:
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+            return None
